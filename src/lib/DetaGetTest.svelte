0fe27910978b10fbb1dc273dc2aee0c78ffacba3
Joe Murphy <murphcle@gmail.com>
2022-01-03 16:43:24 -0500

    added deta loading component

---
+++ src/lib/DetaGetTest.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { getsomething } from './../service/detaService';
+
+	import { onMount } from 'svelte';
+
+	let response;
+	onMount(async () => {
+		const resp = await getsomething();
+		console.log('resp ' + resp);
+		response = resp;
+	});
+</script>
+
+{response}
