d5a8aebbb68e44925628c11937041ea1e4ed93f6
Joe Murphy <murphcle@gmail.com>
2022-01-03 10:51:01 -0500

    added Utils and person model

---
+++ src/utils/dateConverter.ts
@@ -0,0 +1,17 @@
+import { cal } from "src/models/calendar"
+import { Months } from "src/models/months"
+
+export class DateConverter {
+    static daysBeforeMonthStarted = (dt: Date): number => {
+        let daysBefore = 0
+        for (let i = 1; i <= dt.getMonth(); i++) {
+            const month = Months[i]
+            console.log(month)
+            daysBefore += cal[month]['days']
+        }
+        return daysBefore
+    }
+
+    static dayOfYear = (dt: Date): number =>
+        this.daysBeforeMonthStarted(dt) + dt.getDate()
+}
