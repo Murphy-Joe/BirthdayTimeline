3e83f22bdb5d5b5c29852b1982069ca8de216db7
Joe Murphy <murphcle@gmail.com>
2022-01-03 16:44:02 -0500

    added form and deta load to index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
+	import BirthdayForm from '$lib/BirthdayForm.svelte';
 	import DayRow from '$lib/DayRow.svelte';
+	import DetaGetTest from '$lib/DetaGetTest.svelte';
 	import MonthRow from '$lib/MonthRow.svelte';
-	import { Months } from './../models/months';
-	import { calendar } from './../stores';
+	// import { getsomething } from './../service/detaService';
 
 	const unitsPerYear = 4;
-	const unitsPerMonth = 30;
 	let innerWidth: number;
 </script>
 
@@ -13,12 +13,18 @@
 
 <div class="calendar-view">
 	<MonthRow {unitsPerYear} {innerWidth} />
-	<DayRow {unitsPerYear} {unitsPerMonth} {innerWidth} />
+	<DayRow {unitsPerYear} {innerWidth} />
 </div>
 
-<!-- <div>{JSON.stringify($calendar)}</div> -->
-<div>{$calendar.January['1']}</div>
-<div>{Months[1]}</div>
+<div>Gimme a Birthday</div>
+
+<BirthdayForm />
+
+<!-- {#await getsomething() then value}
+	<p>the value is {value}</p>
+{/await} -->
+
+<DetaGetTest />
 
 <style>
 	.calendar-view {
