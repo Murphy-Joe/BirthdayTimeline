79f98144ae6b40310520a4190ecd59a2727b8df0
Joe Murphy <murphcle@gmail.com>
2022-01-03 10:50:21 -0500

    changed objects to classes

--- src/models/calendar.ts
+++ src/models/calendar.ts
@@ -1,42 +1,58 @@
-export interface IMonthObject {
-    days: number
-}
-export interface ICalendar {
-    January: IMonthObject,
-    February: IMonthObject,
-    March: IMonthObject,
-    April: IMonthObject,
-    May: IMonthObject,
-    June: IMonthObject,
-    July: IMonthObject,
-    August: IMonthObject,
-    September: IMonthObject,
-    October: IMonthObject,
-    November: IMonthObject,
-    December: IMonthObject
-}
 
-export const cal: ICalendar = {
-    January: { days: 31 },
-    February: { days: 29 },
-    March: { days: 30 },
-    April: { days: 31 },
-    May: { days: 30 },
-    June: { days: 31 },
-    July: { days: 30 },
-    August: { days: 31 },
-    September: { days: 30 },
-    October: { days: 31 },
-    November: { days: 30 },
-    December: { days: 31 }
-}
-
-Object.keys(cal).forEach((month) => {
-    const numDays = cal[month].days
-    for (let i = 0; i < numDays; i++) {
-        cal[month][i + 1] = ['yo']
+export class CalendarByMonth {
+    constructor() {
+        Object.keys(this).forEach((month) => {
+            const numDays = this[month].days
+            for (let i = 0; i < numDays; i++) {
+                this[month][i + 1] = ['yo']
+            }
+        })
     }
-})
+    January = { days: 31 }
+    February = { days: 28 }
+    March = { days: 31 }
+    April = { days: 30 }
+    May = { days: 31 }
+    June = { days: 30 }
+    July = { days: 31 }
+    August = { days: 31 }
+    September = { days: 30 }
+    October = { days: 31 }
+    November = { days: 30 }
+    December = { days: 31 }
+}
+
+export class CalendarByDays {
+    constructor() {
+        for (let i = 1; i < 366; i++) {
+            this[i] = []
+        }
+    }
+}
+
+
+
+// export interface IMonthObject {
+//     days: number
+// }
+
+// export interface ICalendar {
+//     January: IMonthObject,
+//     February: IMonthObject,
+//     March: IMonthObject,
+//     April: IMonthObject,
+//     May: IMonthObject,
+//     June: IMonthObject,
+//     July: IMonthObject,
+//     August: IMonthObject,
+//     September: IMonthObject,
+//     October: IMonthObject,
+//     November: IMonthObject,
+//     December: IMonthObject
+// }
+
+
+
 
 
 
